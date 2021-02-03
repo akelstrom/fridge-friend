a54a9c35a973fcbbbe080bccecf8e02b51465a9d
jbeedle19 <josh.beedle@gmail.com>
2021-02-03 18:33:50 -0500

    connected models to server.js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,1 +1,9 @@
-// CRUD routes for adding inventory to user's fridge?
+const router = require('express').Router();
+const { Category, Inventory } = require('../../models');
+
+// Dummy router method to connect model server
+router.get('/', (req, res) => {
+    res.send('Hit a post route!');
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-/* app.use(require('./controllers/')); */
+app.use(require('./controllers/'));
 
 // Turn on connection to the db and server
 // STOPPED HERE
