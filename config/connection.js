fee194d03d7d294f54184a7f62c3a2b1e06c5a67
jbeedle19 <josh.beedle@gmail.com>
2021-02-02 19:39:09 -0500

    added jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@ const mysql = require('mysql');
 
 // Create Connection
 const db_config = {
-  host: '',
-  user: '',
-  password: '',
-  database: '',
-  port: ''
+  host: 'cis9cbtgerlk68wl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user: DB_USER,
+  password: DB_PW,
+  database: DB_NAME,
+  port: '3306'
 };
 
 module.exports = db_config;
