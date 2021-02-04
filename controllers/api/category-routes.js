8da96939b3fca93cf0d8b756b440054b3f431801
jbeedle19 <josh.beedle@gmail.com>
2021-02-04 09:37:53 -0500

    created/tested GET all/single item for Inventory API CRUD routes

--- controllers/api/category-routes.js
+++ controllers/api/category-routes.js
@@ -79,7 +79,7 @@ router.delete('/:id', (req, res) => {
     })
     .then(dbCategoryData => {
         if (!dbCategoryData) {
-            res.status(404).json({ message: "No user category found with this id!" });
+            res.status(404).json({ message: "No category found with this id!" });
             return;
         }
         res.json(dbCategoryData);
--- controllers/api/inventory-routes.js
+++ controllers/api/inventory-routes.js
@@ -1,9 +1,147 @@
 const router = require('express').Router();
-const { Inventory, User } = require('../../models');
+const { Inventory, Category, User } = require('../../models');
 
-// Dummy router method to connect model server
+// GET all inventory 
 router.get('/', (req, res) => {
-    res.send('Hit a post route!');
+    Inventory.findAll({
+        // Uncomment this when associations are finished!
+        /* include: [
+            // Include User/Category info with the models
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Category
+            }
+        ] */
+    })
+    .then(dbInventoryData => res.json(dbInventoryData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// GET a single inventory item
+router.get('/:id', (req, res) => {
+    Inventory.findOne({
+        where: {
+            id: req.params.id
+        },
+        // Uncomment this when associations are finished!
+        /* include: [
+            // Include User/Category info with the models
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Category
+            }
+        ] */
+    })
+    .then(dbInventoryData => {
+        if (!dbInventoryData) {
+            res.status(404).json({ message: 'No inventory found with this id!' });
+            return;
+        }
+        res.json(dbInventoryData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// GET inventory by category
+router.get('/:category', (req, res) => {
+    Inventory.findOne({
+        where: {
+            category_name: req.params.category
+        },
+        // Uncomment this when associations are finished!
+        /* include: [
+            // Include User/Category info with the models
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Category
+            }
+        ] */
+    })
+    .then(dbInventoryData => {
+        if (!dbInventoryData) {
+            res.status(404).json({ message: 'No inventory found with this category!' });
+            return;
+        }
+        res.json(dbInventoryData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// Create a new inventory item with post
+router.post('/', (req, res) => {
+    // Expects { inventory_name: "Bread" }
+    Inventory.create({
+        Inventory_name: req.body.Inventory_name
+    })
+    .then(dbInventoryData => res.json(dbInventoryData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
+});
+
+// Update an inventory item's name with put
+router.put('/:id', (req, res) => {
+    // Expects { Inventory_name: "Grains" }
+    Inventory.update(
+        {
+            Inventory_name: req.body.Inventory_name
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+    .then(dbInventoryData => {
+        if (!dbInventoryData) {
+            res.status(404).json({ message: "No inventory found with this id!" });
+            return;
+        }
+        res.json(dbInventoryData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// Delete an inventory item
+router.delete('/:id', (req, res) => {
+    Inventory.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbInventoryData => {
+        if (!dbInventoryData) {
+            res.status(404).json({ message: "No inventory found with this id!" });
+            return;
+        }
+        res.json(dbInventoryData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
