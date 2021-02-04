558536b8f91537758dc746c067915ba9eefc8351
jbeedle19 <josh.beedle@gmail.com>
2021-02-03 19:09:17 -0500

    edited connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,3 @@
-// First Attempt at connection
-/* const mysql = require('mysql2');
-
-require('dotenv').config();
-
-// Create Connection
-const db_config = {
-  host: 'cis9cbtgerlk68wl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  user: process.env.DB_USER,
-  password: process.env.DB_PW,
-  database: process.env.DB_NAME,
-  port: '3306'
-};
-
-module.exports = db_config; */
-
-// Second attempt at connection
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
