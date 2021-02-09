759852b74897700cf4fab66b78503135c5d16942
jbeedle19 <josh.beedle@gmail.com>
2021-02-09 11:30:08 -0500

    outlined logout.js

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,1 +1,14 @@
-// javascript logic for logout here
+async function logout() {
+    const response = await fetch('/api/users/logout', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('#logout').addEventListener('click', logout);
