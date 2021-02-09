1a527174798224adc08863e5ef3f5cfb831ec7ae
jbeedle19 <josh.beedle@gmail.com>
2021-02-09 16:20:39 -0500

    working on inventory cards

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,7 +3,25 @@ const { User, Inventory, Category } = require('../models');
 //const withAuth = require('../utils/auth');
 
 router.get('/', /* withAuth, */ (req, res) => {
-    res.render('dashboard');
+    Inventory.findAll({
+        where: {
+            user_id: 1
+        },
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbInventoryData => {
+        const inventories = dbInventoryData.map(inventory => inventory.get({ plain: true }));
+        res.render('dashboard', {inventories, loggedin: true });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
