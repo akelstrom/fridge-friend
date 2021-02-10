24f7b3d2fe28dcf022a0cbc6ddfd1404bfee4d07
akelstrom <akelstrom@gmail.com>
2021-02-09 20:56:48 -0500

    connected login page to homepage and put in a chunnk of test code.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,1 +1,11 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { User, Inventory, Category } = require('../models');
+
 // Routes for homepage/login 
+
+router.get('/login', (req, res) => {
+    res.render("homepage");
+  });
+
+  module.exports = router;
