350beac8a18afb0a1f5ffffa3584f06a60360af2
steftinsley <69828366+steftinsley@users.noreply.github.com>
2021-02-12 13:35:17 -0600

    added date format helper

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,1 +1,8 @@
 // Tests for any handlebars helpers we create
+test('format_date() returns a date string', () => {
+  const date = new Date('2020-03-20 16:12:03');
+
+  expect(format_date(date)).toBe('3/20/2020');
+});
+
+const {format_date} = require('../utils/helpers');
