59bace33f5de7e465500ff578985cd69b2b33668
jbeedle19 <josh.beedle@gmail.com>
2021-02-12 18:30:28 -0500

    username displays on dashboard

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -17,7 +17,6 @@ router.get('/', /* withAuth, */ (req, res) => {
     })
     .then(dbInventoryData => {
         const inventories = dbInventoryData.map(inventory => inventory.get({ plain: true }));
-        console.log(inventories);
         res.render('dashboard', {inventories, loggedin: true });
     })
     .catch(err => {
