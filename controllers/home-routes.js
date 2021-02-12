c9e38d22a0a12f9309dcb4bbe3a00ef29cd53429
akelstrom <akelstrom@gmail.com>
2021-02-12 14:16:09 -0500

    changed routes to /dashboard

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,18 +3,15 @@ const router = require('express').Router();
 // Routes for homepage/login 
 
 router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-      res.redirect('/');
-      return;
-    }
+    // if (req.session.loggedIn) {
+    //   res.redirect('/dashboard');
+    //   return;
+    // }
     res.render('login');
   });
 
   router.get('/signup', (req,res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-      }
+ 
       res.render('signup');
   })
 
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 //const helpers = require('./utils/helpers');
 
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
