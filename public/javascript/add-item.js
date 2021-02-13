f368a74e576b5b2cd2df347ec9ee2d9bf08e89cd
steftinsley <69828366+steftinsley@users.noreply.github.com>
2021-02-13 14:00:05 -0600

    started js for adding an item

--- public/javascript/add-item.js
+++ public/javascript/add-item.js
@@ -1,1 +1,25 @@
 //form contents for adding an item will go here
+
+const addItemFormHandler = async function (event) {
+  event.preventDefault();
+
+  const item = document.querySelector('').value;
+  const qty = document.querySelector('').value;
+  const category = document.querySelector('').value;
+  const expDate = document.querySelector('').value;
+
+  await fetch(``, {
+    method: "PUT",
+    body: JSON.stringify({
+      item,
+      qty,
+      category,
+      expDate,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  alert("Success!");
+  document.location.replace("");
+};
