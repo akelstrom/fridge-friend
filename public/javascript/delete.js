e960bbcd65c66e87c40d81e6359d17f43b82bf42
jbeedle19 <josh.beedle@gmail.com>
2021-02-15 13:19:46 -0500

    completed delete button functionality

---
+++ public/javascript/delete.js
@@ -0,0 +1,24 @@
+const deleteBtn = document.querySelectorAll('#delete-btn');
+
+async function deleteFormHandler(event) {
+    event.preventDefault();
+
+    const id = this.dataset.id;
+    console.log(id);
+
+    const response = await fetch(`/api/inventory/${id}`, {
+        method: 'DELETE',
+    });
+
+    if (response.ok) {
+        window.location.reload();
+    } else {
+        alert(response.statusText);
+    }
+}
+
+deleteBtn.forEach(
+    function(btn) {
+        btn.addEventListener('click', deleteFormHandler);
+    }
+);
