36bb0fe90c69d9258ae09ab789fdf47126943634
Zihao <zihaozhang1@gmail.com>
2021-02-16 17:13:12 -0500

    color code

--- public/javascript/colorcode.js
+++ public/javascript/colorcode.js
@@ -15,4 +15,35 @@ function testEmail() {
         console.log('Food is still fresh!');
     }
 };
-testEmail();
+
+var auditInventory = function(itemEl) {
+    // get date from task element
+    var date = $(itemEl).find("span").text().trim();
+  
+    // convert to moment object at 12:00AM
+    var time = moment(date, "L").set("hour", 0);
+  
+    // remove any old classes from element
+    $(itemEl).removeClass("alert alert-warning alert-danger");
+  
+    // apply new class if task is near/over due date
+    if (moment().isAfter(time)) {
+      $(itemEl).addClass("alert alert-danger");
+    }
+  
+      // apply new class if task is near/over due date
+    if (moment().isAfter(time)) {
+      $(itemEl).addClass("alert alert-danger");
+    } 
+    else if (Math.abs(moment().diff(time, "days")) <= 2) {
+      $(itemEl).addClass("alert alert-warning");
+    }
+  
+    
+  };
+
+setInterval(function() {
+    // code to execute
+  }, (1000 * 60) * 60 * 24);
+
+loadData();
