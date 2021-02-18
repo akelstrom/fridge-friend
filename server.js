3c02a3ba81ee606b96b1d39ef92cc10098b4e448
jbeedle19 <josh.beedle@gmail.com>
2021-02-18 09:18:33 -0500

    adjusted email functions setInterval to fire every 15 mins

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ sequelize.sync({ force: false }).then(() => {
     setInterval(() => {
         sendExpiringEmail();
         sendExpiredEmail();
-    }, 10000);
+    }, 900000);
     app.listen(PORT, () => console.log("Now listening!"));
 });
